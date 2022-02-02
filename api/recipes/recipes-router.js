9ae9c905e3146d4570ae50f7cd73ac7c01cb3283
EstrellaDionis <EstrellaDionis@gmail.com>
2022-02-01 19:55:57 -0500

    created tables in migrations test.js file and added some files to seeds folder

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
+const recipeResource = require("./recipes-model");
 
 router.get("/", async (req, res) => {
   try {
-    const data = await recipeResource.get();
+    const data = await recipeResource.get(req.params.id);
     res.send(data);
   } catch (err) {
     res.send(err.message);
   }
 });
+
+router.get("/:id", async (req, res) => {});
 
 module.exports = router;
